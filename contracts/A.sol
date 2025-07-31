a534ac55f5f0e5e998e796d05f13ada5ac3445b3
Meek Msaki <meek10x@gmail.com>
2025-07-31 10:05:25 -0500

    test contracts

---
+++ contracts/A.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.30;
+
+contract A {
+    uint256 a;
+
+    function snake_case(uint256 num) public pure {
+        uint256 name;
+    }
+
+    function snake_two(uint256 num) public pure {
+        uint256 name;
+    }
+}
---
+++ contracts/CompilationError.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.30;
+
+contract B {
+    uint256 a;
+    errrrror;
+
+    function snake_case(uint256 num) public pure {
+        uint256 name;
+    }
+
+    function snake_two(uint256 num) public pure {
+        uint256 name;
+    }
+}
